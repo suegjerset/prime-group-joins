db5985ad4ab42dbc83e38bce24129b0fefc3bc72
suegjerset <60236574+suegjerset@users.noreply.github.com>
2020-05-12 16:49:28 -0500

    questions 1 through 4

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,11 +1,28 @@
--- Tasks: Base Mode
+-- BASE MODE
 -- 1. Get all customers and their addresses.
+SELECT first_name, last_name, street, city, state, zip
+FROM "customers"
+JOIN "addresses" ON customers.id = addresses.customer_id;
 
 -- 2. Get all orders and their line items (orders, quantity and product).
+SELECT orders.id, quantity, description
+FROM "orders"
+JOIN "line_items" ON orders.id = line_items.order_id
+JOIN "products" on line_items.product_id = products.id;
 
 -- 3. Which warehouses have cheetos?
+SELECT description, warehouse
+FROM "products"
+JOIN warehouse_product ON products.id = warehouse_product.product_id
+JOIN warehouse ON warehouse_product.warehouse_id = warehouse.id
+WHERE description = 'cheetos';
 
 -- 4. Which warehouses have diet pepsi?
+SELECT description, warehouse
+FROM "products"
+JOIN warehouse_product ON products.id = warehouse_product.product_id
+JOIN warehouse ON warehouse_product.warehouse_id = warehouse.id
+WHERE description = 'diet pepsi';
 
 -- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
